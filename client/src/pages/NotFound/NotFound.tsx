beb4a969dd2847baef45357a18fb15560c26f3f9
keeenas7 <keenas@gmail.com>
2024-10-19 14:48:20 -0400

    notFoundPage

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,8 +4,12 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <h1>This page is not available</h1>
+      <img src= "https://content.imageresizer.com/images/memes/Sitting-Wolf-meme-6z9kbk.jpg" width="wheight" ></img>
+
     </Layout>
+
+    
   );
 }
 
