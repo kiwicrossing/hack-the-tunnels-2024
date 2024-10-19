acf5774335064cabeb250d37424341da3e3aae79
keeenas7 <keenas@gmail.com>
2024-10-19 15:37:18 -0400

    pink tabs hell yeah

--- client/src/layouts/Central/Navigation/Tab/Tab.tsx
+++ client/src/layouts/Central/Navigation/Tab/Tab.tsx
@@ -3,14 +3,15 @@ import "./Tab.style.scss";
 interface TabProps {
   label: string;
   highlighted: boolean;
+  color: boolean;
 }
 
-function Tab({ label, highlighted }: TabProps) {
+function Tab({ label, highlighted, color }: TabProps) {
   return (
     <div className="Tab">
-      <div className="Tab__left"></div>
-      <div className="Tab__label">{label}</div>
-      <div className="Tab__right"></div>
+      <div className="Tab__left" color="hotpink"></div>
+      <div className="Tab__label" color="hotpink">{label}</div>
+      <div className="Tab__right" color="hotpink"></div>
     </div>
   );
 }
